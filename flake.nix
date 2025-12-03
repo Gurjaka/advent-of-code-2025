693c103944bc44484fe390948501682de573cae3
Gurjaka <esartia.gurika@gmail.com>
2025-12-03 18:25:13 +0400

    nix: add flake development env

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Flake template by Gurjaka";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
+
+    flake_attributes = forAllSystems (pkgs: rec {
+      common-system-deps = with pkgs; [
+        compiledb
+      ];
+
+      pkgs-wrapped = pkgs.lib.lists.flatten [
+        # Wrap packages into single list e.g system-deps, python-deps, etc...
+        common-system-deps
+      ];
+    });
+  in {
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = flake_attributes.${pkgs.stdenv.hostPlatform.system}.pkgs-wrapped;
+      };
+    });
+  };
+}
